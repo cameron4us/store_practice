0482331e992b498306af17f07be6dfb617ef45c2
Cameron Forras <cameronforras@hotmail.com>
2019-12-02 10:48:02 -0600

    testing routes

--- app.js
+++ app.js
@@ -2,13 +2,17 @@
 
 const express = require('express');
 const connectDB = require('./config/db');
+const routes = require('./routes/api/reptiles');
 
 const app = express();
+
+// Use api routes
+app.use('/', routes);
 
 // Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+//app.get('/', (req, res) => res.send('Hello world!'));
 
 const port = process.env.PORT || 8082;
 
